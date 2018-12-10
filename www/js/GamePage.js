9a17d837514fb041a39e488f8dc03e16f2f6c7c3
Martin <waldemarsson@gmail.com>
2018-12-10 12:58:25 +0100

    Added GamePage

---
+++ www/js/GamePage.js
@@ -0,0 +1,12 @@
+class GamePage extends Component{
+    constructor(){
+        super();
+        this.addRoute('/gamepage', 'Play');
+        this.form = new Form(this);
+        
+    }
+
+
+
+
+}
