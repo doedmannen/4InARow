659ef82b3780853a7d8e1ffa4b06686e922769e3
Kristoffer Katra <doedmannen@aetmej.se>
2018-12-11 16:13:57 +0100

    Adjusted board square to fit horizontal view

--- www/js/FixEverything.js
+++ www/js/FixEverything.js
@@ -7,10 +7,20 @@ class FixEverything{
       let gHeight = gameDom.offsetHeight;
       if(gHeight > gWidth){
         boardDom.style.maxHeight = `${gWidth}px`;
+        boardDom.style.maxWidth = `${gWidth}px`;
+
+        boardDom.style.minHeight = `${gWidth}px`;
+        boardDom.style.minWidth = `${gWidth}px`;
       } else {
-        let diff = (gWidth-gHeight)/2;
+        let diff = ((gWidth-gHeight)/2);
         boardDom.style.maxWidth = `${gHeight}px`;
+        boardDom.style.maxHeight = `${gHeight}px`;
+
+        boardDom.style.minWidth = `${gHeight}px`;
+        boardDom.style.minHeight = `${gHeight}px`;
+
         boardDom.style.marginLeft = `${diff}px`;
+        boardDom.style.marginRight = `-${diff}px`;
       }
     }, 1);
     return ``;
