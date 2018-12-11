a51b596e5dde9d61d4cd4bc686d065a60ff36ffa
Kristoffer Katra <doedmannen@aetmej.se>
2018-12-11 10:07:33 +0100

    Added functions for rematch and new game in GamePage.js

--- www/js/GamePage.js
+++ www/js/GamePage.js
@@ -6,7 +6,19 @@ class GamePage extends Component{
         this.board = new Board();
     }
 
-    
+    rematch(){
+      let player1 = this.game.players[0];
+      let player2 = this.game.players[1];
+      this.game = new Game(player1, player2);
+      this.render();
+    }
+
+    newGame(){
+      delete this.game;
+      this.form = new Form(this);
+      this.render();
+    }
+
     // takes an object with name and score property
     async saveScore(score){
         let scores = await JSON._load('scores.json');
