d72b9ecb0ccf0c03bec4add3c167097f56aa755e
Ted <ted.agner@hotmail.com>
2018-12-12 22:57:55 +0100

    Added winning condition for diagonal win, both ways. Changed variable name "currCord" to "currCol" to be more descriptive of what it does.

--- www/js/Board.js
+++ www/js/Board.js
@@ -20,9 +20,7 @@ class Board extends Component {
 
     // check if for the wining move
     validate(slot) {
-        // Har satt tillfällig direktretur för att testa
-        this.checkVertical(slot);
-        if (this.checkHorizontal(slot)) {
+        if (this.checkHorizontal(slot) || this.checkVertical(slot) || this.checkDiagonal(slot)) {
             return true;
         }
     }
@@ -33,12 +31,11 @@ class Board extends Component {
         for (let j = 0; j <= 4; j++) {
             slotsConnected = 0;
             for (let i = -4; i < 0; i++) {
-                let currCord = slot.col + i + j;
-                if (currCord < this.gameBoard.length && currCord >= 0) {
-                    if (this.gameBoard[currCord][slot.row].player === this.gameBoard[slot.col][slot.row].player) {
+                let currCol = slot.col + i + j;
+                if (currCol < this.gameBoard.length && currCol >= 0) {
+                    if (this.gameBoard[currCol][slot.row].player === this.gameBoard[slot.col][slot.row].player) {
                         slotsConnected++;
                         if (slotsConnected === 4) {
-                            // Om det är fyra i rad
                             return true;
                         }
                     }
@@ -54,8 +51,28 @@ class Board extends Component {
                 if (this.gameBoard[slot.col][slot.row - i].player === this.gameBoard[slot.col][slot.row].player) {
                     slotsConnected++;
                     if (slotsConnected === 4) {
-                        // Om det är fyra i rad
                         return true;
+                    }
+                }
+            }
+        }
+    }
+
+    checkDiagonal(slot) {
+        let slotsConnected = 0;
+        for (let k = 0; k < 2; k++) {
+            for (let j = 0; j <= 4; j++) {
+                slotsConnected = 0;
+                for (let i = -4; i < 0; i++) {
+                    let currCol = slot.col + i + j;
+                    let currRow = (k === 0) ? slot.row - (i + j) : slot.row + (i + j);
+                    if (currCol < this.gameBoard.length && currCol >= 0 && currRow >= 0 && currRow < this.gameBoard[currCol].length) {
+                        if (this.gameBoard[currCol][currRow].player === this.gameBoard[slot.col][slot.row].player) {
+                            slotsConnected++;
+                            if (slotsConnected === 4) {
+                                return true;
+                            }
+                        }
                     }
                 }
             }
