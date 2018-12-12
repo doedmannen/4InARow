3a07153911c433696fd37f1cec7ec9ff1ca33613
Martin <waldemarsson@gmail.com>
2018-12-12 18:54:09 +0100

    Changes to Bot behavior

--- www/js/Game.js
+++ www/js/Game.js
@@ -22,50 +22,26 @@ class Game extends Component{
       // Decides whos turn it is
       let playerTurn = (this.turn % 2);
       let currentPlayer = this.players[playerTurn];
-      if(currentPlayer instanceof Bot){
-        let slot = currentPlayer.getMove();
-        this.botPlayer(slot, currentPlayer);
-      }
+
       return currentPlayer;
     }
 
-    botPlayer(slot, current){
-      let validSlot = this.board.placeDisc(slot, current);
-      if(validSlot){
-        current.discs--;
-        this.turn++;
-        validSlot.render();
-        this.players[0].render();
-        this.players[1].render();
-
-        // Check with board if it was a winning move
-        let validatedTurn = this.board.validate(validSlot);
-
-        if(validatedTurn){
-          // If move was a win
-          this.gameOver = true;
-          this.winner = current;
-          this.game.winningPage = new WinningPage(this.game, winner);
-          this.render();
-        }
-      }
-
-      if((this.players[0].discs + this.players[1].discs) === 0){
-        // If no more discs
-        this.gameOver = true;
-        this.game.winningPage = new WinningPage(this.game);
-        this.game.render();
-      }
-    }
-
-    
     
     // Get input of a slot object
     playTurn(slot){
       // Get player in turn
       let current = this.currentPlayer();
       // Check with board if the move is valid
-      let validSlot = this.board.placeDisc(slot.col, current);
+
+      let validSlot;
+      
+      if(current instanceof Bot){
+        //if current is Bot
+        validSlot = this.board.placeDisc(current.getMove(), current);
+      } else {
+       validSlot = this.board.placeDisc(slot.col, current);
+      }
+
       // Was the move valid?
       if(validSlot){
         current.discs--;
