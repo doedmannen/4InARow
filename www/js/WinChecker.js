424017d3d4d546f1649e68d5c54d082bca1c9d47
Bartosz Kruba <bartosz.kruba@gmail.com>
2018-12-13 14:59:00 +0100

    Color indication of wining combinations

--- www/js/WinChecker.js
+++ www/js/WinChecker.js
@@ -11,13 +11,19 @@ class WinChecker {
     checkHorizontal(slot) {
         let gameboard = slot.board.gameBoard;
         for (let j = 0; j <= 4; j++) {
+            let discs = [];
             let slotsConnected = 0;
             for (let i = -4; i < 0; i++) {
                 let currCol = slot.col + i + j;
                 if (this.checkBoundary(currCol, slot.row, slot)) {
                     if (gameboard[currCol][slot.row].player === gameboard[slot.col][slot.row].player) {
+                        discs.push(gameboard[currCol][slot.row]);
                         slotsConnected++;
                         if (slotsConnected === 4) {
+                            discs.forEach(cur=>{
+                                cur.player = {color: "orange"};
+                                cur.render();
+                            })
                             return true;
                         }
                     }
@@ -29,11 +35,17 @@ class WinChecker {
     checkVertical(slot) {
         let gameboard = slot.board.gameBoard;
         let slotsConnected = 0;
+        let discs = [];
         for (let i = 0; i < 4; i++) {
             if (this.checkBoundary(slot.col, slot.row - i, slot)) {
                 if (gameboard[slot.col][slot.row - i].player === gameboard[slot.col][slot.row].player) {
+                    discs.push(gameboard[slot.col][slot.row - i]);
                     slotsConnected++;
                     if (slotsConnected === 4) {
+                        discs.forEach(cur=>{
+                            cur.player = {color: "orange"};
+                            cur.render();
+                        })
                         return true;
                     }
                 }
@@ -46,13 +58,19 @@ class WinChecker {
         for (let k = 0; k < 2; k++) {
             for (let j = 0; j <= 4; j++) {
                 let slotsConnected = 0;
+                let discs = [];
                 for (let i = -4; i < 0; i++) {
                     let currCol = slot.col + i + j;
                     let currRow = (k === 0) ? slot.row - (i + j) : slot.row + (i + j);
                     if (this.checkBoundary(currCol, currRow, slot)) {
                         if (gameboard[currCol][currRow].player === gameboard[slot.col][slot.row].player) {
+                            discs.push(gameboard[currCol][currRow]);
                             slotsConnected++;
                             if (slotsConnected === 4) {
+                                discs.forEach(cur=>{
+                                    cur.player = {color: "orange"};
+                                    cur.render();
+                                })
                                 return true;
                             }
                         }
