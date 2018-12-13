6820a89b9cc8c5aacad9252107bbc9e324ef8437
Martin <waldemarsson@gmail.com>
2018-12-13 14:50:12 +0100

    Removed some unnecessary code in Form.js

--- www/js/Form.js
+++ www/js/Form.js
@@ -21,7 +21,6 @@ class Form extends Component{
 
         p1.name = this.baseEl.find('.name-player1').val();
         p2.name = this.baseEl.find('.name-player2').val();
-
         p1.type = document.querySelector('.form-player1 input[name="player1-type"]:checked').value;
         p2.type = document.querySelector('.form-player2 input[name="player2-type"]:checked').value;
 
@@ -39,7 +38,6 @@ class Form extends Component{
             $('#p1-invalid').removeClass('d-unset').addClass('d-none');
             validP1 = true;
         }
-
         if(p2.name.length > 20 || p2.name.length < 2){
             $('#p2-invalid').removeClass('d-none').addClass('d-unset');
         } else {
@@ -51,46 +49,23 @@ class Form extends Component{
         this.createGame(p1, p2);
     }
 
-    readValues(){
-        let p1 = {};
-        let p2 = {};
-
-        p1.name = this.baseEl.find('.name-player1').val();
-        p2.name = this.baseEl.find('.name-player2').val();
-
-
-        // Bots have to see gameboard.
-        p1.type = document.querySelector('.form-player1 input[name="player1-type"]:checked').value;
-        p2.type = document.querySelector('.form-player2 input[name="player2-type"]:checked').value;
-
-        this.validate(p1,p2);
-    }
-
     createGame(p1, p2){
         p1.color = 'red';
         p2.color = 'yellow';
 
         let player1, player2;
-
         if(p1.type === 'human'){
             player1 = new Player(p1, this.gamePage);
         } else if (p1.type === 'bot'){
             player1 = new Bot(p1, this.gamePage);
         }
-
         if(p2.type === 'human'){
             player2 = new Player(p2, this.gamePage);
         } else if (p2.type === 'bot'){
             player2 = new Bot(p2, this.gamePage);
         }
-
-        // let player1 = new Player(p1);
-        // let player2 = new Player(p2);
-
         this.gamePage.game = new Game(player1, player2, this.gamePage);
         this.gamePage.render();
         FixEverything.navbar.render(); 
-
-
     }
 }
